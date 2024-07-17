b33fbe9fb03a244a5e59c92d8f492c06b4a49ff4
hadiset <hadiset420@gmail.com>
2024-07-17 16:59:56 +0700

    slow fetching data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,11 +17,11 @@ export async function fetchRevenue() {
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    // const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
 
-    // return data.rows;
+    return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
